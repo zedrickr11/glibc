de0cb6b1710575df490b4dfef2adeb6807e1664b
Zedrick Rodriguez <inedc4zedrickr@gmail.com>
2018-12-20 11:49:11 -0600

    crud personas (maestros)

---
+++ app/Http/Controllers/PersonaController.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests\PersonaFormRequest;
+use App\Persona;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Input;
+
+class PersonaController extends Controller
+{
+
+      /**
+       * Display a listing of the resource.
+       *
+       * @return \Illuminate\Http\Response
+       */
+      public function index()
+      {
+        $persona=Persona::all();
+        return view ('persona.index',compact('persona'));
+      }
+
+      /**
+       * Show the form for creating a new resource.
+       *
+       * @return \Illuminate\Http\Response
+       */
+      public function create()
+      {
+          return view('persona.create');
+      }
+
+      /**
+       * Store a newly created resource in storage.
+       *
+       * @param  \Illuminate\Http\Request  $request
+       * @return \Illuminate\Http\Response
+       */
+      public function store(PersonaFormRequest $request)
+      {
+        $persona=new Persona;
+        $persona->nombres=$request->get('nombres');
+        $persona->apellidos=$request->get('apellidos');
+        $persona->email=$request->get('email');
+        $persona->fechanacimiento=$request->get('fechanacimiento');
+        $persona->estado_civil=$request->get('estado_civil');
+        $persona->nacionalidad=$request->get('nacionalidad');
+        $persona->profesion=$request->get('profesion');
+        $persona->dpi=$request->get('dpi');
+        $persona->direccion=$request->get('direccion');
+        $persona->telefono=$request->get('telefono');
+        $persona->telefono_dos=$request->get('telefono_dos');
+        $persona->celular=$request->get('celular');
+
+        if (Input::hasFile('foto')){
+          $file=Input::file('foto');
+          $file->move(public_path().'/personas/fotos/',$file->getClientOriginalName());
+            $persona->foto=$file->getClientOriginalName();
+        }
+        $persona->tipo_persona="maestro";
+
+        $persona->save();
+        return redirect()->route('persona.index');
+      }
+
+      /**
+       * Display the specified resource.
+       *
+       * @param  int  $id
+       * @return \Illuminate\Http\Response
+       */
+      public function show($id)
+      {
+        $persona=Persona::findOrFail($id);
+        return view('persona.show', compact('persona'));
+      }
+
+      /**
+       * Show the form for editing the specified resource.
+       *
+       * @param  int  $id
+       * @return \Illuminate\Http\Response
+       */
+      public function edit($id)
+      {
+        $persona=Persona::findOrFail($id);
+        return view('persona.edit',compact('persona'));
+      }
+
+      /**
+       * Update the specified resource in storage.
+       *
+       * @param  \Illuminate\Http\Request  $request
+       * @param  int  $id
+       * @return \Illuminate\Http\Response
+       */
+      public function update(PersonaFormRequest $request, $id)
+      {
+          $persona=Persona::findOrFail($id);
+          $persona->nombres=$request->get('nombres');
+          $persona->apellidos=$request->get('apellidos');
+          $persona->email=$request->get('email');
+          $persona->fechanacimiento=$request->get('fechanacimiento');
+          $persona->estado_civil=$request->get('estado_civil');
+          $persona->nacionalidad=$request->get('nacionalidad');
+          $persona->profesion=$request->get('profesion');
+          $persona->dpi=$request->get('dpi');
+          $persona->direccion=$request->get('direccion');
+          $persona->telefono=$request->get('telefono');
+          $persona->telefono_dos=$request->get('telefono_dos');
+          $persona->celular=$request->get('celular');
+
+          if (Input::hasFile('foto')){
+            $file=Input::file('foto');
+            $file->move(public_path().'/personas/fotos/',$file->getClientOriginalName());
+              $persona->foto=$file->getClientOriginalName();
+          }
+          $persona->tipo_persona="maestro";
+          $persona->update();
+          return redirect()->route('persona.index');
+      }
+
+      /**
+       * Remove the specified resource from storage.
+       *
+       * @param  int  $id
+       * @return \Illuminate\Http\Response
+       */
+      public function destroy($id)
+      {
+
+        $persona = Persona::findOrFail($id);
+        $persona->condicion = '0';
+        $persona->update();
+        return redirect()->route('persona.index');
+      }
+}
---
+++ resources/views/persona/create.blade.php
@@ -0,0 +1,114 @@
+@extends ('layouts.admin')
+@section ('contenido')
+
+    <!-- Breadcrumb -->
+    <ol class="breadcrumb">
+      <li class="breadcrumb-item">GlibColegio</li>
+      <li class="breadcrumb-item"><a href="#">Persona</a></li>
+      <li class="breadcrumb-item active">Nuevo</li>
+      <!-- Breadcrumb Menu-->
+
+    </ol>
+
+    <div class="container-fluid">
+      <div class="animated fadeIn">
+        <div class="row">
+          <div class="col-sm-12">
+
+            <div class="card">
+              <div class="card-header">
+                <strong>Persona</strong>
+                <small>Form</small>
+              </div>
+              <form class="" action="{{ route('persona.store') }}" method="post" enctype="multipart/form-data">
+                {!! csrf_field() !!}
+
+              <div class="card-body">
+                <div class="row">
+                  <div class="form-group col-md-6">
+                    <label for="nombres">Nombres</label>
+                    <input type="text" class="form-control" name="nombres" placeholder="Nombres...">
+                    {!!$errors->first('nombres','<span class=text-danger>:message</span>')!!}
+                  </div>
+
+                  <div class="form-group col-md-6">
+                    <label for="apellidos">Apellidos</label>
+                    <input type="text" class="form-control" name="apellidos" placeholder="Apellidos...">
+                    {!!$errors->first('apellidos','<span class=text-danger>:message</span>')!!}
+                  </div>
+                  <div class="form-group col-md-6">
+                    <label for="email">Correo electrónico</label>
+                    <input type="email" class="form-control" name="email" placeholder="Correo electrónico...">
+                    {!!$errors->first('email','<span class=text-danger>:message</span>')!!}
+                  </div>
+                  <div class="form-group col-md-6">
+                    <label for="fechanacimiento">Fecha de nacimiento</label>
+                    <input type="date" class="form-control" name="fechanacimiento">
+                    {!!$errors->first('fechanacimiento','<span class=text-danger>:message</span>')!!}
+                  </div>
+                  <div class="form-group col-md-4">
+                    <label for="estado_civil">Estado civil</label>
+                    <input type="text" class="form-control" name="estado_civil" placeholder="Estado civil...">
+                    {!!$errors->first('estado_civil','<span class=text-danger>:message</span>')!!}
+                  </div>
+                  <div class="form-group col-md-4">
+                    <label for="nacionalidad">Nacionalidad</label>
+                    <input type="text" class="form-control" name="nacionalidad" placeholder="Nacionalidad...">
+                    {!!$errors->first('nacionalidad','<span class=text-danger>:message</span>')!!}
+                  </div>
+                  <div class="form-group col-md-4">
+                    <label for="profesion">Profesión</label>
+                    <input type="text" class="form-control" name="profesion" placeholder="Profesión...">
+                    {!!$errors->first('profesion','<span class=text-danger>:message</span>')!!}
+                  </div>
+                  <div class="form-group col-md-4">
+                    <label for="dpi">Documento de Identifiación</label>
+                    <input type="text" class="form-control" name="dpi" placeholder="Documento de Identifiación...">
+                    {!!$errors->first('dpi','<span class=text-danger>:message</span>')!!}
+                  </div>
+                  <div class="form-group col-md-8">
+                    <label for="direccion">Dirección</label>
+                    <input type="text" class="form-control" name="direccion" placeholder="Dirección...">
+                    {!!$errors->first('direccion','<span class=text-danger>:message</span>')!!}
+                  </div>
+                  <div class="form-group col-md-4">
+                    <label for="telefono">Teléfono</label>
+                    <input type="text" class="form-control" name="telefono" placeholder="Teléfono...">
+                    {!!$errors->first('telefono','<span class=text-danger>:message</span>')!!}
+                  </div>
+                  <div class="form-group col-md-4">
+                    <label for="telefono_dos">Teléfono alternativo</label>
+                    <input type="text" class="form-control" name="telefono_dos" placeholder="Teléfono alternativo...">
+                    {!!$errors->first('telefono_dos','<span class=text-danger>:message</span>')!!}
+                  </div>
+                  <div class="form-group col-md-4">
+                    <label for="celular">Celular</label>
+                    <input type="text" class="form-control" name="celular" placeholder="Celular...">
+                    {!!$errors->first('celular','<span class=text-danger>:message</span>')!!}
+                  </div>
+                  <div class="form-group col-md-4">
+                    <label for="foto">Foto</label>
+                    <input type="file" class="form-control" name="foto" placeholder="Celular...">
+                    {!!$errors->first('foto','<span class=text-danger>:message</span>')!!}
+                  </div>
+                </div>
+
+
+
+
+              </div>
+
+              <div class="card-footer">
+                <a href="{{ route('seccion.index') }}"> <button type="button" class="btn btn-sm btn-success"><i class="fa fa-toggle-left"></i> Atrás</button></a>
+                <button type="submit" class="btn btn-sm btn-primary"><i class="fa fa-dot-circle-o"></i> Guardar</button>
+                <button type="reset" class="btn btn-sm btn-danger"><i class="fa fa-ban"></i> Cancelar</button>
+              </div>
+              </form>
+            </div>
+
+          </div>
+        </div>
+      </div>
+
+    </div>
+@endsection
