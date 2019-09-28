a3e90aa1df59785d7afd62032fdbc02555927bdb
Ferch8 <f-er1016@hotmail.com>
2019-09-28 10:22:18 -0600

    agregar director al middleware en controlador padres

--- app/Http/Controllers/PadreController.php
+++ app/Http/Controllers/PadreController.php
@@ -18,7 +18,7 @@ class PadreController extends Controller
   public function __construct()
    {
        $this->middleware('auth');
-       $this->middleware('role:admin');
+       $this->middleware('role:admin,director');
    }
 
 
