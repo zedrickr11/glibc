6e3daede4e6a8e5c213a41b341d2128a7560bed0
Ferch8 <f-er1016@hotmail.com>
2019-09-28 10:22:36 -0600

    agregar director al middleware del controlador de personas

--- app/Http/Controllers/PersonaAdminController.php
+++ app/Http/Controllers/PersonaAdminController.php
@@ -21,7 +21,7 @@ class PersonaAdminController extends Controller
     public function __construct()
      {
          $this->middleware('auth');
-         $this->middleware('role:admin');
+         $this->middleware('role:admin,director');
      }
 
 
